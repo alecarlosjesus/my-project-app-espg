aa8ad887f0e5262b50993778c78d07e647c38c8a
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-19 11:15:50 -0300

    Exemplos de hooks, useEffectx3 + useState.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
 //BLOCO DE ROTAS
 import { createBrowserRouter,RouterProvider } from 'react-router-dom';
@@ -23,7 +22,5 @@ const router = createBrowserRouter([
 //BLOCO DE ROTAS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <RouterProvider router={router}/>
-  </React.StrictMode>,
 )
