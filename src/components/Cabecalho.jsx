e82f7b6b0044464344c245682ebdb09870c64c71
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-26 11:24:40 -0300

    Iniciando estilo com CSS.MODULE no Cabecalho.

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,17 +1,21 @@
 import { Link } from "react-router-dom";
+import styles from "./Cabecalho.module.css";
 
 export default function Cabecalho() {
+
+  
   return (
     <>
-        <header>
+        <header className={styles.cabecalho}>
           <h1>Vite + React</h1>
+          <img src="/img/produtos.png" alt="Mãos segurando caixas." /> 
 
           {/* Crie uma lista com 5 links para as nossas rotas:
           Obs: Utilize o componente Link do router-dom */}
 
           <nav>
             <ul>
-              <li><Link to="/">HOME</Link> </li>
+              <li><Link to="/">HOME</Link></li>
               <li><Link to="/produtos">PRODUTOS</Link> </li>
               <li><Link to="/editar/produtos/1">Produto - 1</Link> </li>
               <li><Link to="/editar/produtos/2">Produto - 2</Link> </li>
