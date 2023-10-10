1efdabed9f5f0f7042dd4b70f84328b212b3c5cb
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-10-10 11:31:13 -0300

    Form editarProdutos criado.

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,15 +1,49 @@
 
 import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export default function EditarProdutos() {
 
     //Recuperando o parâmetro ID com o HOOK useParams();
-
+    document.title = "EDITAR PRODUTOS";
     const {id} = useParams();
+    
+    useEffect(() => {
 
-    document.title = "EDITAR PRODUTOS " + id;
+      fetch(`http://localhost:5000/produtos/${id}`)
+        .then((response)=> response.json())
+        .then((response)=> console.log(response))
+        .catch(error=> console.log(error))
 
+    }, [id])
+    
   return (
-    <div>EditarProdutos</div>
+    <div>
+      <h1>EDITAR PRODUTOS</h1>
+
+      <div>
+        <form>
+          <fieldset>
+            <legend>Produto Selecionado</legend>
+            <div>
+              <label htmlFor="idNome">Nome</label>
+              <input type="text" name="nome" id="idNome" placeholder="Digite o nome do produto"/>
+            </div>
+            <div>
+              <label htmlFor="idDesc">Descrição</label>
+              <input type="text" name="desc" id="idDesc" placeholder="Digite a descrição do produto"/>
+            </div>
+            <div>
+              <label htmlFor="idPreco">Valor</label>
+              <input type="number" name="preco" id="idPreco" placeholder="Digite o valor do produto"/>
+            </div>
+            <div>
+              <button>EDITAR</button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+
+    </div>
   )
 }
